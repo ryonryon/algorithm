4361ca9444acca6654553cad43e1c1961a109896
ryotogashi <ryo.togashi.ca@gmail.com>
2019-10-12 22:51:44 -0700

    add insertion sort.
    Signed-off-by: ryotogashi <ryo.togashi.ca@gmail.com>

---
+++ src/sorting/InsertionSort.ts
@@ -0,0 +1,39 @@
+function InsertionSort<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    let smallestElementIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[smallestElementIndex]) {
+        smallestElementIndex = j;
+      }
+    }
+    Shift(arr, smallestElementIndex, i);
+  }
+}
+
+function Shift<T>(arr: T[], shiftIndex: number, to: number): void {
+  let temp = arr[shiftIndex];
+  for (let i = shiftIndex; to < i; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[to] = temp;
+}
+
+describe("InsertionSort", () => {
+  test("#1", () => {
+    const numberArray = [1, 4, 12, 67, 3, 98, 1001, 55, 34];
+    InsertionSort(numberArray);
+    expect(numberArray).toEqual([1, 3, 4, 12, 34, 55, 67, 98, 1001]);
+  });
+  test("#2", () => {
+    const stringArray = ["ryo", "rei", "togashi", "ton", "apple", "banana"];
+    InsertionSort(stringArray);
+    expect(stringArray).toEqual([
+      "apple",
+      "banana",
+      "rei",
+      "ryo",
+      "togashi",
+      "ton"
+    ]);
+  });
+});
