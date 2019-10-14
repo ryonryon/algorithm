566dbc95577abb2cbf65304cb9ead92d0ab3bd79
ryotogashi <ryo.togashi.ca@gmail.com>
2019-10-14 10:36:09 -0700

    add merge sort and edit bubble sort pic
    Signed-off-by: ryotogashi <ryo.togashi.ca@gmail.com>

---
+++ src/sorting/MergeSort.ts
@@ -0,0 +1,53 @@
+function MergeSort<T>(arr: T[]): T[] {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = MergeSort([...arr.slice(0, middle)]);
+  const right = MergeSort([...arr.slice(middle)]);
+
+  return marge(left, right);
+}
+
+function marge<T>(left: T[], right: T[]): T[] {
+  const mergedArr = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      mergedArr.push(left[i]);
+      i++;
+    } else {
+      mergedArr.push(right[j]);
+      j++;
+    }
+  }
+
+  if (i === left.length) {
+    mergedArr.push(...right.slice(j));
+  } else {
+    mergedArr.push(...left.slice(i));
+  }
+
+  return mergedArr;
+}
+
+describe("MargeSort", () => {
+  test("#1", () => {
+    const numberArray = [1, 4, 12, 67, 3, 98, 1001, 55, 34];
+    expect(MergeSort(numberArray)).toEqual([1, 3, 4, 12, 34, 55, 67, 98, 1001]);
+  });
+  test("#2", () => {
+    const stringArray = ["ryo", "rei", "togashi", "ton", "apple", "banana"];
+    expect(MergeSort(stringArray)).toEqual([
+      "apple",
+      "banana",
+      "rei",
+      "ryo",
+      "togashi",
+      "ton"
+    ]);
+  });
+});
--- src/sorting/README.md
+++ src/sorting/README.md
@@ -1,6 +1,6 @@
 ## Bubble Sort
 
-![Bubble Sort](https://user-images.githubusercontent.com/35892492/66712201-edd5fe80-ed4d-11e9-84b6-6a5f6951e79e.jpg)
+![Bubble Sort](https://user-images.githubusercontent.com/35892492/66767492-70051680-ee65-11e9-887d-d3e2a3ac13ba.jpg)
 
 ## Insertion Sort
 
